1a0ab0abaec96de53d5fc483be5eb29331f577ae
Virgile Cheminot <virgile.cheminot@gmail.com>
2021-12-20 13:56:07 +0100

    yup

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
 const reportDiv = document.getElementById("lb_report");
 const menuBar = document.getElementById("lb_menubar");
 const parentDiv = reportDiv.parentElement;
-const displayWidth = window.innerWidth;
+const width = 1000;
 const sectionTitles = document.getElementsByClassName("lb_part_title_actions");
-
-const maxWidth = 1000;
 
 parentDiv.style.position = "absolute";
 parentDiv.style.width = "100%";
@@ -13,15 +11,14 @@ parentDiv.style.flexDirection = "column";
 parentDiv.style.alignItems = "center";
 
 reportDiv.style.width = "95%";
-reportDiv.style.maxWidth = maxWidth + 'px';
+reportDiv.style.maxWidth = width + 'px';
 reportDiv.style.margin = "0 15px";
 
 menuBar.style.width = "100%";
-menuBar.style.maxWidth = maxWidth + 15 + 'px';
+menuBar.style.maxWidth = width + 15 + 'px';
 
 for (let i = 0; i < sectionTitles.length; i++) {
     const element = sectionTitles[i];
     element.removeAttribute = "100%";
     element.style.width = "100%";
-    console.log(element)
-}
+};
